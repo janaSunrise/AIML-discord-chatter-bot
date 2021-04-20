450cbfc1bc2a3b88b042697e3ab16e4d2f26b89a
janaSunrise <warriordefenderz@gmail.com>
2021-04-20 18:07:24 +0530

    Added AIML Files, Get everything working.

--- bot/cogs/core/__init__.py
+++ bot/cogs/core/__init__.py
@@ -3,7 +3,6 @@ from collections import Counter
 from bot import Bot
 
 from .error import ErrorHandler
-from .help import Help
 from .sudo import Sudo
 
 
@@ -13,5 +12,4 @@ def setup(bot: Bot) -> None:
         bot.socket_stats = Counter()
 
     bot.add_cog(ErrorHandler(bot))
-    bot.add_cog(Help(bot))
     bot.add_cog(Sudo(bot))
